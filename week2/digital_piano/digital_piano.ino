d24327554e0a32501710aed1ed6a68e554325623
Tannu <tannu121105@gmail.com>
2026-07-02 17:42:22 +0530

    git add . git commit -m "v1.2: added chord support for multiple button press"

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -12,20 +12,31 @@ void setup() {
   pinMode(btn4, INPUT_PULLUP);
 }
 
-void loop()else {
-  noTone(buzzer);
-} {
+void loop() {
 
-  if (digitalRead(btn1) == LOW) {
+  // 👇 INPUT READ (Step 6 added)
+  bool b1 = digitalRead(btn1) == LOW;
+  bool b2 = digitalRead(btn2) == LOW;
+  bool b3 = digitalRead(btn3) == LOW;
+  bool b4 = digitalRead(btn4) == LOW;
+
+  // 🎹 CHORD + NOTES LOGIC
+  if ((b1 + b2 + b3 + b4) >= 2) {
+    tone(buzzer, 392); // Sol (Chord)
+  }
+  else if (b1) {
     tone(buzzer, 262); // Do
   }
-  else if (digitalRead(btn2) == LOW) {
+  else if (b2) {
     tone(buzzer, 294); // Re
   }
-  else if (digitalRead(btn3) == LOW) {
+  else if (b3) {
     tone(buzzer, 330); // Mi
   }
-  else if (digitalRead(btn4) == LOW) {
+  else if (b4) {
     tone(buzzer, 349); // Fa
+  }
+  else {
+    noTone(buzzer); // stop sound
   }
 }
