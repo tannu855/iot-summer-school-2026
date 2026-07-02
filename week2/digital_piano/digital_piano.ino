649e9214df0ecde7981f0fa862e57595bfca1533
Tannu <tannu121105@gmail.com>
2026-07-02 17:51:48 +0530

    v1.3: added major/minor scale toggle using mode button

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -4,39 +4,54 @@ const int btn1 = 2;
 const int btn2 = 3;
 const int btn3 = 4;
 const int btn4 = 5;
+const int modeBtn = 6;
+
+bool minorMode = false;
 
 void setup() {
   pinMode(btn1, INPUT_PULLUP);
   pinMode(btn2, INPUT_PULLUP);
   pinMode(btn3, INPUT_PULLUP);
   pinMode(btn4, INPUT_PULLUP);
+  pinMode(modeBtn, INPUT_PULLUP);
 }
 
 void loop() {
 
-  // 👇 INPUT READ (Step 6 added)
+  // 🎚 Mode toggle button
+  if (digitalRead(modeBtn) == LOW) {
+    minorMode = !minorMode;
+    delay(300); // debounce
+  }
+
   bool b1 = digitalRead(btn1) == LOW;
   bool b2 = digitalRead(btn2) == LOW;
   bool b3 = digitalRead(btn3) == LOW;
   bool b4 = digitalRead(btn4) == LOW;
 
-  // 🎹 CHORD + NOTES LOGIC
+  // 🎼 Notes (Major / Minor)
+  int doNote = minorMode ? 247 : 262;
+  int reNote = 294;
+  int miNote = minorMode ? 311 : 330;
+  int faNote = 349;
+
+  // 🎹 Chord logic
   if ((b1 + b2 + b3 + b4) >= 2) {
-    tone(buzzer, 392); // Sol (Chord)
+    tone(buzzer, 392);
   }
   else if (b1) {
-    tone(buzzer, 262); // Do
+    tone(buzzer, doNote);
   }
   else if (b2) {
-    tone(buzzer, 294); // Re
+    tone(buzzer, reNote);
   }
   else if (b3) {
-    tone(buzzer, 330); // Mi
+    tone(buzzer, miNote);
   }
   else if (b4) {
-    tone(buzzer, 349); // Fa
+    tone(buzzer, faNote);
   }
   else {
-    noTone(buzzer); // stop sound
+    noTone(buzzer);
   }
 }
