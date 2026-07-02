827fdcc212455af66edcae27225843951c22bd41
Tannu <tannu121105@gmail.com>
2026-07-02 17:21:44 +0530

    feat: add basic digital piano

---
+++ week2/digital_piano/digital_piano.ino
@@ -0,0 +1,29 @@
+const int buzzer = 8;
+
+const int btn1 = 2;
+const int btn2 = 3;
+const int btn3 = 4;
+const int btn4 = 5;
+
+void setup() {
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+  pinMode(btn3, INPUT_PULLUP);
+  pinMode(btn4, INPUT_PULLUP);
+}
+
+void loop() {
+
+  if (digitalRead(btn1) == LOW) {
+    tone(buzzer, 262); // Do
+  }
+  else if (digitalRead(btn2) == LOW) {
+    tone(buzzer, 294); // Re
+  }
+  else if (digitalRead(btn3) == LOW) {
+    tone(buzzer, 330); // Mi
+  }
+  else if (digitalRead(btn4) == LOW) {
+    tone(buzzer, 349); // Fa
+  }
+}
